d2a67e855f1423bf1ec86493f6a612068fa7abc3
Anisha <bollapanisha@gmail.com>
2020-12-16 16:57:43 -0800

    ichange

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, i1;
 var backgroundImg,platform;
 
 function preload() {
@@ -23,6 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    i1 = new Box(300,200,70,70);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    
 }
